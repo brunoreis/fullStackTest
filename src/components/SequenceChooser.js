ee551a069e3d4a23b44cdd4759e5acef2324a633
Bruno Reis <bruno.reis@scalablepath.com>
2019-01-09 21:29:16 -0200

    sequence choice visual behavior - ok

--- src/components/SequenceChooser.js
+++ src/components/SequenceChooser.js
@@ -1,27 +1,63 @@
 import React from 'react';
+import { withState, compose } from 'recompose';
+import PropTypes from 'prop-types';
 
-export default () =>
-  <div>
-    <button type="button" className="btn btn-primary">
-      Choose sequencer.
-    </button>
-    <div className="AddSequencer">
-      <ul className="list-group InnerText">
-        <a href="#" className="list-group-item list-group-item-action">
-          Factorial
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          Fibonacci
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          Range
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          Prime
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          Partial Sum
-        </a>
-      </ul>
-    </div>
+const SEQUENCES = [
+  {name:"Factorial", sequence: () => {}},
+  {name:"Fibonacci", sequence: () => {}},
+  {name:"Range", sequence: () => {}},
+  {name:"Prime", sequence: () => {}},
+  {name:"Partial Sum", sequence: () => {}},
+]
+
+const ListItem = ({name, sequence, onClick}) =>
+  <a href="#" {...{onClick}} className="list-group-item list-group-item-action">
+    {name}
+  </a>
+
+const List = ({
+  setActivatedSequence,
+}) =>
+  <div className="AddSequencer">
+    <ul className="list-group InnerText">
+      {
+        SEQUENCES.map(
+          (sequenceData) => <ListItem
+            onClick = {() => setActivatedSequence(sequenceData)}
+            {...sequenceData}
+          />
+        )
+      }
+    </ul>
   </div>
+
+const Button = ({onClick}) =>
+  <button {...{onClick}} type="button" className="btn btn-primary">
+    Choose sequencer.
+  </button>
+
+const SequenceChooser = ({
+  choosing,
+  setChoosing,
+  setActivatedSequence,
+}) =>
+  <div>
+    {
+      choosing
+        ? <List {...{setActivatedSequence}}/>
+        : <Button
+            onClick={() => setChoosing(true)}
+          />
+      // @todo: transform onClick in handler to avoid double rendering
+    }
+  </div>
+
+const SmartButton = compose(
+  withState('choosing', 'setChoosing', false),
+)(SequenceChooser)
+
+SmartButton.propTypes = {
+  setActivatedSequence: PropTypes.func.isRequired,
+}
+
+export default SmartButton;
